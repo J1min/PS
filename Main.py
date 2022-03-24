ad7d06d1983466046d8f52ac9081eb74a074b8f1
J1min <jmhong101506@gmail.com>
2022-03-24 16:57:29 +0900

    브론즈1

--- Main.py
+++ Main.py
@@ -1,18 +1,8 @@
-n = input().upper()
-check = list(0 for i in range(26))
-b = -1
-count = 0
-
-for i in n:
-    check[ord(i)-65] += 1
-
-for i in range(26):
-    if(check[i] > b):
-        b = check[i]
-        count = i
-
-if check.count(max(check))>1:
-    print("?")
-else:
-    print(chr(count+65))
-
+n = input()
+f = int(input())
+a = int(n[:-2] + '00')
+while True:
+    if a % f == 0:
+        break
+    a += 1
+print(str(a)[-2:])
--- Temp.py
+++ Temp.py
@@ -1,20 +1,6 @@
-n = input()
-arr = []
-check = list(0 for i in range(26))
-now1 = 0
-max1 = -1
-
-for i in n:
-    arr.append(i)
-
-for i in arr:
-    now = ord(i)-65
-    if now >= 32:
-        now -= 32
-    print("ord : ",now)
-    check[now] += 1
-
-
-for i in range(26):
-  print(check[i]<max1)
-print(max1)
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i,j,k = command
+        answer.append(list(sorted(array[i-1:j]))[k-1])
+    return answer
