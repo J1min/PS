08eec09eab30fe339ca46c63cea83fb4c5cecde0
J1min <jmhong101506@gmail.com>
2022-04-11 12:05:26 +0900

    순열

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-pass
+input_list = [1, 2, 3, 4, 5]
+used = [0]*len(input_list)
+
+print(used)  # [0, 0, 0, 0 ,0]
+
+
+def perm(arr, n):
+    if n == len(input_list):
+        print(arr)
+        return
+    for i in range(len(input_list)):
+        if not used[i]:
+            used[i] = 1
+            arr.append(input_list[i])
+            perm(arr, n+1)
+            arr.pop()
+            used[i] = 0
+
+
+perm([], 0)
