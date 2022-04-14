3499029393552873623dbd840b1f354431865235
J1min <jmhong101506@gmail.com>
2022-04-14 19:09:03 +0900

    엄준식을찾아라

--- Main.py
+++ Main.py
@@ -1,20 +1,31 @@
-input_list = [1, 2, 3, 4, 5]
-used = [0]*len(input_list)
+import math
+n = int(input())
 
-print(used)  # [0, 0, 0, 0 ,0]
+# 1	1	6 14
+# 2	2	7 21
+# 3	4	8 22
+# 4	11	9 24
+# 5	12	10 41
+
+n10 = n % 100
+n1 = n % 10
+
+n10 /= 3
+n1 %= 3
+
+n10 = math.floor(n10)
+n1 = math.floor(n1)
 
 
-def perm(arr, n):
-    if n == len(input_list):
-        print(arr)
-        return
-    for i in range(len(input_list)):
-        if not used[i]:
-            used[i] = 1
-            arr.append(input_list[i])
-            perm(arr, n+1)
-            arr.pop()
-            used[i] = 0
+if n1 == 0:
+    n1 = 1
 
+if n10 >= 2 & n1 == 4:
+    n10 -= 1
+    n1 = 3
 
-perm([], 0)
+if n10 % 3 == 0:
+    n10 -= 1
+    
+
+print(n10, n1, end="")
