c7b116deb755dbbfe875ff32206982b968a22a6c
J1min <jmhong101506@gmail.com>
2022-04-18 16:44:34 +0900

    순회

--- Main.py
+++ Main.py
@@ -1,31 +1,16 @@
-import math
-n = int(input())
+def Permutation(length):
+    n = length
+    perm = list()  # 0 for _ in range(n)
+    numList = list(i for i in range(1, length+1))  # 나열해야하는 수의 목록
 
-# 1	1	6 14
-# 2	2	7 21
-# 3	4	8 22
-# 4	11	9 24
-# 5	12	10 41
+    if len(perm) == n or len(numList) == 0:
+        print(perm)
+        return
 
-n10 = n % 100
-n1 = n % 10
+    for i in numList:
+        perm.append(i)
+        numList2 = numList - [i]
+        Permutation(perm, numList2)
+        perm.delete(i)
 
-n10 /= 3
-n1 %= 3
-
-n10 = math.floor(n10)
-n1 = math.floor(n1)
-
-
-if n1 == 0:
-    n1 = 1
-
-if n10 >= 2 & n1 == 4:
-    n10 -= 1
-    n1 = 3
-
-if n10 % 3 == 0:
-    n10 -= 1
-    
-
-print(n10, n1, end="")
+Permutation(3)
