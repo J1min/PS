1f5a2b38cb15657d332661fab0c197f10bb9f3dd
J1min <aoiharu41@gmail.com>
2022-06-02 15:52:14 +0900

    123

--- Main.py
+++ Main.py
@@ -1,16 +1,1 @@
-def Permutation(length):
-    n = length
-    perm = list()  # 0 for _ in range(n)
-    numList = list(i for i in range(1, length+1))  # 나열해야하는 수의 목록
-
-    if len(perm) == n or len(numList) == 0:
-        print(perm)
-        return
-
-    for i in numList:
-        perm.append(i)
-        numList2 = numList - [i]
-        Permutation(perm, numList2)
-        perm.delete(i)
-
-Permutation(3)
+print(a)
