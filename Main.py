4fc81e7d889eef910bc92b097dc6e0764887a721
J1min <aoiharu41@gmail.com>
2022-06-08 23:49:55 +0900

    파이썬 2653

--- Main.py
+++ Main.py
@@ -1,17 +1,18 @@
 n = int(input()) # 3, 이진수의 길이
-arr = ['0'] # 길이에 맞는 이진수들을 넣는 배열
+arr = [] # 길이에 맞는 이진수들을 넣는 배열
 i = 0 # 반복에 쓰일 친구
 count = 0
 
 while 1:
-  i += 1
-  if len(bin(i)) <= n+2:
-    arr.append(bin(i)[2:]) # ob를 뺀 진짜 이진수들을 모아놓음
-  elif len(bin(i)) >= n+2:
-    print(arr)
+  binary = bin(i)[2:]
+  if len(binary) <= n:
+    arr.append(binary.zfill(n)) # ob를 뺀 진짜 이진수들을 모아놓음
+  elif len(binary) > n:
     for j in arr:
-      if j not in "00":
+      if "00" not in j:
         count += 1
     break
+  i += 1
+  
 
 print(count)
