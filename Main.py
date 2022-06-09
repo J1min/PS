c12206a1a3f0d34a20560a845ef880eaa8a472ac
J1min <aoiharu41@gmail.com>
2022-06-09 17:57:06 +0900

    우박스ㅜ

--- Main.py
+++ Main.py
@@ -1,18 +1,13 @@
-n = int(input()) # 3, 이진수의 길이
-arr = [] # 길이에 맞는 이진수들을 넣는 배열
-i = 0 # 반복에 쓰일 친구
-count = 0
+def solution(n):
+  i = 0
+  while True:
+    if n == 1:
+      return i
+    if i >= 500:
+      return -1
 
-while 1:
-  binary = bin(i)[2:]
-  if len(binary) <= n:
-    arr.append(binary.zfill(n)) # ob를 뺀 진짜 이진수들을 모아놓음
-  elif len(binary) > n:
-    for j in arr:
-      if "00" not in j:
-        count += 1
-    break
-  i += 1
-  
-
-print(count)
+    if n % 2 == 0:
+      n /= 2
+    else:
+      n = n * 3 + 1
+    i += 1
