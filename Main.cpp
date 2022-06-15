7ceb0020bbb3b9728b8c2c663f6dd7c6f0ad7d4f
J1min <aoiharu41@gmail.com>
2022-06-15 13:01:21 +0900

    UPDATE :: 헤더파일 수정

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-int num;
-
-int d(int x) {
-
-	return num;
-}
-
 int main()
 {
-	int n;
-	vector<int> v;
 	
 }
