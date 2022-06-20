be2b96057a19af1b533bc12ff572ecb47097c55c
J1min <jmhong101506@gmail.com>
2022-06-20 10:46:08 +0900

    S4 달성

--- Main.py
+++ Main.py
@@ -1,13 +1,21 @@
-n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-dp = [0 for i in range(n)]
+from collections import Counter
 
-for i in range(n):
-    for j in range(i):
-        if a[i] > a[j] and dp[i] < dp[j]:
-            dp[i] = dp[j]
-    dp[i] += 1
-print(n - max(dp)) # 줄 세우기
-print(max(dp)) # 증가하는 부분 수열  
+numbers = []
+for _ in range(int(input())):
+    num = int(input())
+    numbers.append(num)
+
+numbers.sort()
+
+cnt = Counter(numbers).most_common(2)
+
+print(round(sum(numbers) / len(numbers)))
+print(numbers[len(numbers) // 2])
+if len(numbers) > 1:
+    if cnt[0][1] == cnt[1][1]:
+        print(cnt[1][0])
+    else:
+        print(cnt[0][0])
+else:
+    print(cnt[0][0])
+print(max(numbers) - min(numbers))
--- Pro.py
+++ Pro.py
@@ -1,29 +1,6 @@
-n = int(input())
-s = []
-dp = [0 for i in range(n)]
-for i in range(n):
-    s.append(int(input())) # 
-dp[0] = 1
-
-for i in range(1, n):
-    a = []
-    for j in range(i):
-        if s[i] > s[j]:
-            a.append(dp[j])
-    if not a:
-        dp[i] = 1
-    else:
-        dp[i] = max(a) + 1
-print(n - max(dp))
-
-# ----------------------------------------------------------------
-
-n = int(input())
-a = list(map(int, input().split()))
-dp = [0 for i in range(n)]
-for i in range(n):
-    for j in range(i):
-        if a[i] > a[j] and dp[i] < dp[j]:
-            dp[i] = dp[j]
-    dp[i] += 1
-print(n - max(dp))
+arr = [1, 3, 8, -2, 2, 3]
+arr_cnt = []
+for i in arr:
+    cnt = arr.count(i)
+    arr.append(cnt)
+print(arr_cnt)
