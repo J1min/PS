40dec202ecbb2a5585ee43559f1ff824365f58b4
J1min <jmhong101506@gmail.com>
2022-06-20 09:43:39 +0900

    UPDATE :: 백준 실버V 90%

--- Main.py
+++ Main.py
@@ -3,9 +3,11 @@ a = []
 for i in range(n):
     a.append(int(input()))
 dp = [0 for i in range(n)]
+
 for i in range(n):
     for j in range(i):
         if a[i] > a[j] and dp[i] < dp[j]:
             dp[i] = dp[j]
     dp[i] += 1
-print(n - max(dp))
+print(n - max(dp)) # 줄 세우기
+print(max(dp)) # 증가하는 부분 수열  
