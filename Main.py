d17cee9a480e66d6825f2890ef503ba539a9b792
J1min <jmhong101506@gmail.com>
2022-06-22 00:40:03 +0900

    ADD :: 브루트포스 알고리즘 입문

--- Main.py
+++ Main.py
@@ -1,14 +1,11 @@
-import sys
-input = sys.stdin.readline
-arr = []
-n = int(input().strip())
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+sum1 = 0
+min1 = []
+arr2 = []
 for i in range(n):
-    q = input().strip()
-    if q not in arr:
-        arr.append(q)
-arr = sorted(arr)
-print(arr)
-arr = sorted(arr, key=len)
-print(arr)
-# for i in arr:
-#     print(i)
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if n > (i + j + k):
+                min1.append(m-(arr[i] + arr[j] + arr[k]))
+print(min(min1))
