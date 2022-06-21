3659faddce92e9012db0dbfd0a2623b637a305cc
J1min <jmhong101506@gmail.com>
2022-06-22 00:12:06 +0900

    ADD :: 백준 이상한정렬 풀이

--- Main.py
+++ Main.py
@@ -1,16 +1,14 @@
-def solution(lottos, win_nums):
-    best = 0
-    worst = 0
-    for i in lottos:
-        if i in win_nums or i == 0:
-            best += 1
-    bestGrade = 7 - best
-    worstGrade = 7 - (best - lottos.count(0))
-
-    if bestGrade == 7:
-        bestGrade -= 1
-    if worstGrade == 7:
-        worstGrade -= 1
-    return [bestGrade, worstGrade]
-
-print(solution([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]))
+import sys
+input = sys.stdin.readline
+arr = []
+n = int(input().strip())
+for i in range(n):
+    q = input().strip()
+    if q not in arr:
+        arr.append(q)
+arr = sorted(arr)
+print(arr)
+arr = sorted(arr, key=len)
+print(arr)
+# for i in arr:
+#     print(i)
