94b5635ab50069f2fa47372f58584865bd5ab7de
J1min <jmhong101506@gmail.com>
2022-06-24 03:01:53 +0900

    ADD :: C++ 공부한 것들

--- Main.cpp
+++ Main.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
 using namespace std;
-int n;
-int m;
-int price[20];
-int preference[20];
-// ver3. repetitive dp + sliding window
-// 초밥 가격 100의 배수 -> 가격을 100으로 나누기
-// 모든 초밥의 가격은 2만원 이하 -> 가격을 100으로 나누었으므로 200원 이하
-// -> budget-200 ~ budget 까지 계산된 값을 저장할 공간이 필요함 -> 201개의 메모리 필요
-int sw[201];
-int FindMaxPref_sliding_window()
+
+char *MakeStrAdr(int len)
 {
-	m = m / 100; // 총 예산 100으로 나누기
-	sw[0] = 0;
-	for(int budget=1 ; budget <= m ; budget++)
-	{
-		sw[budget%201] = 0;
-		for(int i = 0 ; i < n ; i++)
-		{
-			int targetPrice = price[i]/100; // 스시 가격 100으로 나누기
-			int targetPref = preference[i];
-			if(budget < targetPrice) continue;
-			sw[budget%201] = max(sw[budget%201], sw[(budget-targetPrice)%201]+targetPref);
-		}
-	}
-	return sw[m%201];
+	char* str = new char[len];
+	return str;
 }
 
 int main(void)
 {
-	cin >> n >> m;
-	for(int i = 0 ; i < m ; i++)
-		cin >> price[i] >> preference[i];
-	cout << FindMaxPref_sliding_window() << endl;
+	char *str = MakeStrAdr(20);
+	strcpy(str, "I am so happy~");
+	cout << str << endl;
+	delete str;
 	return 0;
 }
--- Pro.py
+++ Pro.py
@@ -1,6 +1,22 @@
-arr = [1, 3, 8, -2, 2, 3]
-arr_cnt = []
-for i in arr:
-    cnt = arr.count(i)
-    arr.append(cnt)
-print(arr_cnt)
+def permutation(arr, r):
+    # 1.
+    arr = sorted(arr)
+    used = [0 for _ in range(len(arr))]
+
+    def generate(chosen, used):
+        # 2.
+        if len(chosen) == r:
+            print(chosen)
+            return
+	
+	# 3.
+        for i in range(len(arr)):
+            if not used[i]:
+                chosen.append(arr[i])
+                used[i] = 1
+                generate(chosen, used)
+                used[i] = 0
+                chosen.pop()
+    generate([], used)
+
+permutation([1, 2, 3, 4, 5], 3)
