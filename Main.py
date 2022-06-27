d24ea54c65fa4b5a7ce61d1d1bd300d693bb9299
J1min <jmhong101506@gmail.com>
2022-06-28 08:42:22 +0900

    ADD :: 각 자릿수 합 더하기 알고리즘

--- Main.py
+++ Main.py
@@ -1,9 +1,2 @@
-n, m, k = map(int, input().split())
-arr = list(map(int, input().split()))
-arr = sorted(arr)
-next_max = m % k
-real_max = 0
-if next_max == 0:
-  next_max = m - k
-real_max = m - next_max
-print((arr[-1] * real_max) + (arr[-2] * next_max))
+number = 11
+print(sum(map(int,str(number))))
