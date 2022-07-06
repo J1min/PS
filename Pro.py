e0ce8e6ac0e52d98366e3481e1b6a77b5399934b
J1min <jmhong101506@gmail.com>
2022-07-06 19:17:15 +0900

    ADD :: 백준 S2 달성

--- Pro.py
+++ Pro.py
@@ -1,4 +1,12 @@
-n1 = int(input())
-for i in range(n1):
-    n, m = map(int, input().split())
-    answer = n * m % 10
+dictionary = {"A+": 4.5,
+              "A0": 4.0,
+              "B+": 3.5,
+              "B0": 3.0,
+              "C+": 2.5,
+              "C0": 2.0,
+              "D+": 1.5,
+              "D0": 1.0,
+              "F": 0.0}
+
+
+print(type(dictionary["A+"]))
--- TempCodeRunnerFile.py
+++ TempCodeRunnerFile.py
@@ -1,12 +1,5 @@
-_ = input()
-sum = 0
-n = list(input())
+N = int(input())
 arr = []
-for i in n:
-    arr.append(ord(i)-96)
-
-for i in range(len(arr)):
-    sum += arr[i] * (31 ** i)
-    sum %= 1234567891
-
-print(sum)
+for i in range(N):
+    q = list(input())
+    
