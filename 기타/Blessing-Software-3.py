dcf506717e9cb1b31c9f1d5f506973d757b8758d
J1min <jmhong101506@gmail.com>
2022-07-11 22:54:55 +0900

    ADD :: 동아리 코테 상급 추가

--- 기타/Blessing-Software-3.py
+++ 기타/Blessing-Software-3.py
@@ -1,4 +1,10 @@
 number = list(map(int, input().split()))
-for i in range(1, len(number)-2):
-    if number[i] == 0:
-        number.pop(i+1)
+number2 = []
+
+for i in range(1, len(number)):
+    if number[i] != 0:
+        number2.append(number[i])
+    else:
+        number2.pop()    
+        
+print(sum(number2))
