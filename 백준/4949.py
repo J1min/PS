fd69ee04375692689432cf4ea6907a3cf278873f
J1min <jmhong101506@gmail.com>
2022-07-14 23:24:56 +0900

    REACHED :: 실버1 달성

--- 백준/4949.py
+++ 백준/4949.py
@@ -1,9 +1,22 @@
-while True:
-    arr = []
-    q = input()
-    for i in q:
-        if i == "(":
-            arr.append(i)
-        elif i == ")":
-            arr.pop()
-    print(arr)
+q = "0"
+while q != '.':
+    try:
+        arr = []
+        q = input()
+        for i in q:
+            if i == "(" and arr[-1] == ")":
+                arr.append(i)
+                print("yes")
+            elif i == ")" and arr[-1] == "(": 
+                arr.pop()
+                print("yes")
+            elif i == "[" and arr[-1] == "]":
+                arr.append(i)
+                print("yes")
+            elif i == "]" and arr[-1] == "[":
+                arr.pop()
+                print("yes")
+            else:
+                print("no")
+    except IndexError:
+        print("no")
