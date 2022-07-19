8599d19fa72d98acc79eb62b2687369a735ec15f
J1min <jmhong101506@gmail.com>
2022-07-19 17:54:45 +0900

    ADD :: 막대기 SOLVE

--- 백준/.Temp.py
+++ 백준/.Temp.py
@@ -1,2 +1,25 @@
-import string
-print(list(string.ascii_lowercase))
+X = int(input())
+count = 0
+if X % 64 != X:
+    X %= 64
+    count += 1
+if X % 32 != X:
+    X %= 32
+    count += 1
+if X % 16 != X:
+    X %= 16
+    count += 1
+if X % 8 != X:
+    X %= 8
+    count += 1
+if X % 4 != X:
+    X %= 4
+    count += 1
+if X % 2 != X:
+    X %= 2
+    count += 1
+
+if X % 2 == 1:
+    print(count + 1)
+else:
+    print(count)
