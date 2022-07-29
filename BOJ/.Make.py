7b74ea7e08f2bdbfa09843543622de7117b88d6f
J1min <jmhong101506@gmail.com>
2022-07-29 16:21:10 +0900

    FIX :: Make 파일 리팩토링

--- BOJ/.Make.py
+++ BOJ/.Make.py
@@ -8,10 +8,13 @@ if not Make.is_file():
     f = open(f"BOJ/{Number}.py", "w")
     f.write(f"import sys\n")
     f.write(f"# PROBLEM - \n")
+    f.write(f"# TIER - G\n")
     f.write(f"# NUMBER - {Number}\n")
     f.write(f"# DATE - {str(datetime.today())[:16]}\n")
     f.write(f"# IDEA - \n")
-
+    f.write(f"#\n")
+    f.write(f"#\n")
+    
     print(f"\n{Number}.py 만들엇서요\n")
     print(f"BOJ/{Number}.py")
     print(f"BOJ/{Number}.py")
