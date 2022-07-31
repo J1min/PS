a418e254b96e5ce372a77a52abfcea8bdfd9dafe
J1min <jmhong101506@gmail.com>
2022-07-31 13:41:05 +0900

    SOLVE :: 힙 + 그리디

--- BOJ/.Temp.py
+++ BOJ/.Temp.py
@@ -1,18 +1,3 @@
-start = 1
-def set_wallet(money):
-    global start
-    wallet = [start]
-    while start < money:
-        if len(str(start)) % 2 == 1:
-            start *= 10
-            wallet.append(start)
-        elif str(start)[:2] == '25':
-            start *= 4
-            wallet.append(start)
-        else:
-            start *= 25
-            start //= 10
-            wallet.append(start)
-    wallet.pop()
-    return wallet
-print(set_wallet(47))
+import heapq
+array = sorted([1,21,3,4,5,35,5,4,3,5,98,21,14,17,32])
+print(array)
--- BOJ/13975.py
+++ BOJ/13975.py
@@ -1,9 +1,26 @@
-import queue
+import sys
+# PROBLEM - 파일 합치기 3
+# TIER - G4
+# NUMBER - 13975
+# DATE - 2022-07-30 18:21
+# IDEA - 그리디
+# 이건 내일 풀거긴 한데 이거 아무리 봐도
+# 힙 써서 최소값(힙으로 쓰는 배열 제일 처음 값) 계속 pop 해가면서 재끼는 문제
 
-heap = queue.PriorityQueue()
-
-N = int(input())
-for i in range(N):
-    M = int(input())
+import heapq
+input = sys.stdin.readline
+T = int(input().rstrip())
+for i in range(T):
+    answer = []
+    N = int(input().rstrip())
     numbers = list(map(int, input().split()))
-    
+    heapq.heapify(numbers)
+    while True:
+        if len(numbers) <= 1:
+            print(sum(answer))
+            break
+        min_ = heapq.heappop(numbers)
+        max_ = heapq.heappop(numbers)
+        result = min_ + max_
+        heapq.heappush(numbers, result)
+        answer.append(result)
